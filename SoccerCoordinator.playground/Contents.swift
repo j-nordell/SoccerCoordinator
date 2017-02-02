15c6ce2d171a41f221c39fbdeb91258d2f5c0ba2
jen-nordell <jennifer.nordell@outlook.com>
2017-02-02 03:42:28 +0100

    Fixes Issue #2  - Implementation of constants to guard against typos in keys when manually entering the dictionaries

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -4,99 +4,107 @@
 
 import UIKit
 
+// Implementation of constants to guard against
+// typos in keys
+
+let name = "name"
+let height = "height"
+let experience = "experience"
+let guardians = "guardians"
+
 // Single collection that contains all 18 players
 // Each player is its own dictionary stored in an array
 
 let players = [
-    ["name": "Joe Smith",
-     "height": 42,
-     "experience": true,
-     "guardians": "Jim and Jan Smith"
+    [name: "Joe Smith",
+     height: 42,
+     experience: true,
+     guardians: "Jim and Jan Smith"
      ],
-    ["name": "Jill Tanner",
-     "height": 36,
-     "experience": true,
-     "guardians": "Clara Tanner"
+    [name: "Jill Tanner",
+     height: 36,
+     experience: true,
+     guardians: "Clara Tanner"
     ],
-    ["name": "Bill Bon",
-     "height": 43,
-     "experience": true,
-     "guardians": "Sara and Jenny Bon"
+    [name: "Bill Bon",
+     height: 43,
+     experience: true,
+     guardians: "Sara and Jenny Bon"
     ],
-    ["name": "Eva Gordon",
-     "height": 45,
-     "experience": false,
-     "guardians": "Wendy and Mike Gordon"
+    [name: "Eva Gordon",
+     height: 45,
+     experience: false,
+     guardians: "Wendy and Mike Gordon"
     ],
-    ["name": "Matt Gill",
-     "height": 40,
-     "experience": "false",
-     "guardians": "Charles and Sylvia Gill"
+    [name: "Matt Gill",
+     height: 40,
+     experience: "false",
+     guardians: "Charles and Sylvia Gill"
     ],
-    ["name": "Kimmy Stein",
-     "height": 41,
-     "experience": false,
-     "guardians": "Bill and Hillary Stein"
+    [name: "Kimmy Stein",
+     height: 41,
+     experience: false,
+     guardians: "Bill and Hillary Stein"
     ],
-    ["name": "Sammy Adams",
-     "height": 45,
-     "experience": false,
-     "guardians": "Jeff Adams"
+    [name: "Sammy Adams",
+     height: 45,
+     experience: false,
+     guardians: "Jeff Adams"
     ],
-    ["name": "Karl Saygan",
-     "height": 42,
-     "experience": true,
-     "guardians": "Heather Bledsoe"
+    [name: "Karl Saygan",
+     height: 42,
+     experience: true,
+     guardians: "Heather Bledsoe"
     ],
-    ["name": "Suzanne Greenberg",
-     "height": 44,
-     "experience": true,
-     "guardians": "Henrietta Dumas"
+    [name: "Suzanne Greenberg",
+     height: 44,
+     experience: true,
+     guardians: "Henrietta Dumas"
     ],
-    ["name": "Sal Dali",
-     "height": 41,
-     "experience": false,
-     "guardians": "Gala Dali"
+    [name: "Sal Dali",
+     height: 41,
+     experience: false,
+     guardians: "Gala Dali"
     ],
-    ["name": "Joe Kavalier",
-     "height": 39,
-     "experience": false,
-     "guardians": "Sam and Elaine Kavalier"
+    [name: "Joe Kavalier",
+     height: 39,
+     experience: false,
+     guardians: "Sam and Elaine Kavalier"
     ],
-    ["name": "Ben Finkelstein",
-     "height": 44,
-     "experience": false,
-     "guardians": "Aaron and Jill Finkelstein"
+    [name: "Ben Finkelstein",
+     height: 44,
+     experience: false,
+     guardians: "Aaron and Jill Finkelstein"
     ],
-    ["name": "Diego Soto",
-     "height": 41,
-     "experience": true,
-     "guardians": "Robin and Sarika Soto"
+    [name: "Diego Soto",
+     height: 41,
+     experience: true,
+     guardians: "Robin and Sarika Soto"
     ],
-    ["name": "Chloe Alaska",
-     "height": 47,
-     "experience": false,
-     "guardians": "David and Jamie Alaska"
+    [name: "Chloe Alaska",
+     height: 47,
+     experience: false,
+     guardians: "David and Jamie Alaska"
     ],
-    ["name": "Arnold Willis",
-     "height": 43,
-     "experience": false,
-     "guardians": "Claire Willis"
+    [name: "Arnold Willis",
+     height: 43,
+     experience: false,
+     guardians: "Claire Willis"
     ],
-    ["name": "Phillip Helm",
-     "height": 44,
-     "experience": true,
-     "guardians": "Thomas Helm and Eva Jones"
+    [name: "Phillip Helm",
+     height: 44,
+     experience: true,
+     guardians: "Thomas Helm and Eva Jones"
     ],
-    ["name": "Les Clay",
-     "height": 42,
-     "experience": true,
-     "guardians": "Wynonna Brown"
+    [name: "Les Clay",
+     height: 42,
+     experience: true,
+     guardians: "Wynonna Brown"
     ],
-    ["name": "Herschel Krustofski",
-     "height": 45,
-     "experience": true,
-     "guardians": "Hyman and Rachel Krustofski"
+    [name: "Herschel Krustofski",
+     height: 45,
+     experience: true,
+     guardians: "Hyman and Rachel Krustofski"
     ]
 ]
 
