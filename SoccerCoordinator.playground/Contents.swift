84bda013d01498d0005e3149e9c6d1c92cea7536
jen-nordell <jennifer.nordell@outlook.com>
2017-02-02 23:16:06 +0100

    fixing strange git behaviour

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -11,7 +11,8 @@ let name = "name"
 let height = "height"
 let experience = "experience"
 let guardians = "guardians"
-
+let yes = "yes"
+let no = "no"
 
 // Single collection that contains all 18 players
 // Each player is its own dictionary stored in an array
@@ -19,92 +20,92 @@ let guardians = "guardians"
 let players = [
     [name: "Joe Smith",
      height: 42,
-     experience: true,
+     experience: yes,
      guardians: "Jim and Jan Smith"
-     ],
+    ],
     [name: "Jill Tanner",
      height: 36,
-     experience: true,
+     experience: yes,
      guardians: "Clara Tanner"
     ],
     [name: "Bill Bon",
      height: 43,
-     experience: true,
+     experience: yes,
      guardians: "Sara and Jenny Bon"
     ],
     [name: "Eva Gordon",
      height: 45,
-     experience: false,
+     experience: no,
      guardians: "Wendy and Mike Gordon"
     ],
     [name: "Matt Gill",
      height: 40,
-     experience: false,
+     experience: "no",
      guardians: "Charles and Sylvia Gill"
     ],
     [name: "Kimmy Stein",
      height: 41,
-     experience: false,
+     experience: no,
      guardians: "Bill and Hillary Stein"
     ],
     [name: "Sammy Adams",
      height: 45,
-     experience: false,
+     experience: no,
      guardians: "Jeff Adams"
     ],
     [name: "Karl Saygan",
      height: 42,
-     experience: true,
+     experience: yes,
      guardians: "Heather Bledsoe"
     ],
     [name: "Suzanne Greenberg",
      height: 44,
-     experience: true,
+     experience: yes,
      guardians: "Henrietta Dumas"
     ],
     [name: "Sal Dali",
      height: 41,
-     experience: false,
+     experience: no,
      guardians: "Gala Dali"
     ],
     [name: "Joe Kavalier",
      height: 39,
-     experience: false,
+     experience: no,
      guardians: "Sam and Elaine Kavalier"
     ],
     [name: "Ben Finkelstein",
      height: 44,
-     experience: false,
+     experience: no,
      guardians: "Aaron and Jill Finkelstein"
     ],
     [name: "Diego Soto",
      height: 41,
-     experience: true,
+     experience: yes,
      guardians: "Robin and Sarika Soto"
     ],
     [name: "Chloe Alaska",
      height: 47,
-     experience: false,
+     experience: no,
      guardians: "David and Jamie Alaska"
     ],
     [name: "Arnold Willis",
      height: 43,
-     experience: false,
+     experience: no,
      guardians: "Claire Willis"
     ],
     [name: "Phillip Helm",
      height: 44,
-     experience: true,
+     experience: yes,
      guardians: "Thomas Helm and Eva Jones"
     ],
     [name: "Les Clay",
      height: 42,
-     experience: true,
+     experience: yes,
      guardians: "Wynonna Brown"
     ],
     [name: "Herschel Krustofski",
      height: 45,
-     experience: true,
+     experience: yes,
      guardians: "Hyman and Rachel Krustofski"
     ]
 ]
